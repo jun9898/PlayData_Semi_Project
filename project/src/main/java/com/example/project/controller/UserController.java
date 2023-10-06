3b892725b55e2051ab1fb3f8ffe42c7241a27629
jun9898 <nice1998@gmail.com>
2023-10-06 13:39:36 +0900

    Feat : Mypage와 follow list 뷰 연동
    - UserController에서 뷰 연동

--- project/src/main/java/com/example/project/controller/UserController.java
+++ project/src/main/java/com/example/project/controller/UserController.java
@@ -8,22 +8,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/user")
-public class UserTestController {
+public class UserController {
 
     @Autowired
     UserTestService userTestService;
 
 //    테스트 코드로 user/list의 버튼에서 status값을 가져와 해당 값에 해당하는 정보를 뿌려주는 로직입니다.
-    @RequestMapping("/{name}")
-    public String TestPath(@PathVariable("name") String name, String status, Model model){
+//    @RequestMapping("/{name}")
+//    public String TestPath(@PathVariable("name") String name, String status, Model model){
+//        if (status != null){
+//            model.addAttribute("list",userTestService.testLogic(status));
+//        }
+//        return "user/"+name;
+//    }
+
+    @RequestMapping("/list")
+    public String mypage(String status, Model model){
         if (status != null){
             model.addAttribute("list",userTestService.testLogic(status));
         }
-        return "user/"+name;
+        return "user/list";
     }
 
     @RequestMapping("/mypage")
