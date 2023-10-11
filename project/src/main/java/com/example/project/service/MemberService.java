b12a82672b4f1391c34d94a11a4520eb31890087
jun9898 <nice1998@gmail.com>
2023-10-11 21:13:41 +0900

    Remove : 파일 정리
    - 사용하지 않는 파일 정리 및 디렉토리 구조 재정렬
    - mapper, entity 디렉토리 추가

--- project/src/main/java/com/example/project/service/MemberService.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.service;
-
-import com.example.project.dto.UserDTO;
-
-public interface MemberService {
-    void join(UserDTO member);
-    UserDTO findMember(Long memberId);
-}
--- project/src/main/java/com/example/project/service/UserTestService.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.service;
-
-import java.util.List;
-
-
-public interface UserTestService {
-    List testLogic(String status);
-}
